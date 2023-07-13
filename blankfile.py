2ecdef91146c2043305ba29be4d1523a42e47d4e
Eugeny Zubkov <zubkoves@smsol.ru>
2023-07-13 23:11:37 +0300

    Sprint 2, task 3

---
+++ blankfile.py
@@ -0,0 +1,22 @@
+from math import sqrt
+
+message = ('Добро пожаловать в самую лучшую программу для вычисления '
+           'квадратного корня из заданного числа')
+
+
+def calculate_square_root(Number):
+    """Вычисляет квадратный корень."""
+    return sqrt(Number)
+
+
+def calc(your_number):
+    """Сообщение квадратный корень."""
+    number = calculate_square_root(your_number)
+    if your_number <= 0:
+        return
+    print(f'Мы вычислили квадратный корень из введённого вами числа. '
+          f'Это будет: {number}')
+
+
+print(message)
+calc(25.5)
